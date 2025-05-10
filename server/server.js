24ed830f6371355b241f0f4bb2b7182408516864
Ritik Patil <ritikpatil566@gmail.com>
2025-05-11 00:25:28 +0530

    feat: add health check API

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import morgan from 'morgan';
+import morgan from "morgan";
 
 import cityRoutes from "./routes/cities.route.js";
 import vehicleRoutes from "./routes/vehicles.route.js";
@@ -11,9 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get("/health", (req, res) => {
+  return res.status(200).json({ message: "Server is up!" });
+});
 
 app.use("/api/cities", cityRoutes);
 app.use("/api/vehicle", vehicleRoutes);
