165faa5c3e80afc900b0813d5501f1970498568d
Ritik Patil <ritikpatil566@gmail.com>
2025-05-10 15:41:37 +0530

    feat: constant file

---
+++ client/src/utils/utils.js
@@ -0,0 +1,5 @@
+export const copsData = [
+  { id: 1, name: "Officer Johnson", cityId: null, vehicleId: null },
+  { id: 2, name: "Detective Miller", cityId: null, vehicleId: null },
+  { id: 3, name: "Inspector Garcia", cityId: null, vehicleId: null },
+];
